b305ef28b2911d9706ac1e75e98032dc2e508cc5
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-05-23 13:31:48 -0700

    Refactor main header nav

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hamburgerMenuButton = document.querySelector(".toggle-menu-button");
 const hamburgerMenuIcon = document.querySelector(".hamburger-icon");
-const mobileMenu = document.querySelector(".nav-mobile-container");
+const mobileMenu = document.querySelector(".main-header__nav");
 
 hamburgerMenuButton.addEventListener("click", () => {
   hamburgerMenuIcon.classList.toggle("opacity-50");
